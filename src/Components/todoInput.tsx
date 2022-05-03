38d97bc27b3640150e19538ace1a1a41f433c2e3
nitipat21 <nitipat.rstw@gmail.com>
2022-05-03 22:01:29 +0700

    create logic to limit amount of doCard

--- src/Components/todoInput.tsx
+++ src/Components/todoInput.tsx
@@ -1,23 +1,40 @@
 import * as React from 'react';
-import { useDispatch } from 'react-redux';
-import { actions } from '../store';
+import { useDispatch, useSelector } from 'react-redux';
+import { actions, RootState } from '../store';
 
 const TodoInput:React.FC = () => {
 
     const dispatch = useDispatch();
 
+    const doItemsListState = useSelector((state:RootState) => state.todo.doItemsList)
+
     const [doInput,setDoInput] = React.useState('');
 
+    const [warnTodoInput,isWarnTodoInput] = React.useState(false);
+
     const addTodo = () => {
-        dispatch(actions.addTodo(doInput));
-        setDoInput('');
-    };
+
+        if(doItemsListState.length === 10 || doInput === '') {
+            
+            isWarnTodoInput(true);
+
+            setTimeout(()=> {
+                isWarnTodoInput(false)
+            },1500);
+
+        } else {
+            dispatch(actions.addTodo(doInput));
+            setDoInput('');
+        }
+    } 
+    
+    
 
     return (
 
         <div className="todoInput-container">
             <div className='todoInputText-container'>
-                <input id='inputText'type='text' onChange={(event)=> setDoInput(event.target.value)} value={doInput} placeholder='What are you gonna do?'/>
+                <input className={ warnTodoInput ? 'shaking' : "" } id='inputText'type='text' onChange={(event)=> setDoInput(event.target.value)} value={doInput} placeholder='What are you gonna do?'/>
             </div>
             <div className='todoInputButton-container'>
                 <button type='button' onClick={addTodo}>Add</button>
